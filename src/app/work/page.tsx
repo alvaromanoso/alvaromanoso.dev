b5156b29437a2399705ae2a85d5154fe2a688e51
Álvaro Mañoso Oca <43934777+alvaromanoso@users.noreply.github.com>
2025-08-02 20:53:50 +0200

    Canonicals (#24)

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,69 +1,74 @@
-import { getPosts } from "@/app/utils/utils";
-import { Column } from "@/once-ui/components";
-import { Projects } from "@/components/work/Projects";
-import { baseURL } from "@/app/resources";
-import { person, work } from "@/app/resources/content";
+import {getPosts} from "@/app/utils/utils";
+import {Column} from "@/once-ui/components";
+import {Projects} from "@/components/work/Projects";
+import {baseURL} from "@/app/resources";
+import {person, work} from "@/app/resources/content";
+import {Metadata} from "next";
 
-export async function generateMetadata() {
-  const title = work.title;
-  const description = work.description;
-  const ogImage = `https://${baseURL}/og?title=${encodeURIComponent(title)}`;
+export async function generateMetadata(): Promise<Metadata> {
+    const title = work.title;
+    const description = work.description;
+    const ogImage = `https://${baseURL}/og?title=${encodeURIComponent(title)}`;
+    const canonical = `https://${baseURL}/work`;
 
-  return {
-    title,
-    description,
-    openGraph: {
-      title,
-      description,
-      type: "website",
-      url: `https://${baseURL}/work/`,
-      images: [
-        {
-          url: ogImage,
-          alt: title,
+    return {
+        title,
+        description,
+        openGraph: {
+            title,
+            description,
+            type: "website",
+            url: `https://${baseURL}/work/`,
+            images: [
+                {
+                    url: ogImage,
+                    alt: title,
+                },
+            ],
         },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title,
-      description,
-      images: [ogImage],
-    },
-  };
+        twitter: {
+            card: "summary_large_image",
+            title,
+            description,
+            images: [ogImage],
+        },
+        alternates: {
+            canonical
+        }
+    };
 }
 
 export default function Work() {
-  let allProjects = getPosts(["src", "app", "work", "projects"]);
+    let allProjects = getPosts(["src", "app", "work", "projects"]);
 
-  return (
-    <Column maxWidth="m">
-      <script
-        type="application/ld+json"
-        suppressHydrationWarning
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "CollectionPage",
-            headline: work.title,
-            description: work.description,
-            url: `https://${baseURL}/projects`,
-            image: `${baseURL}/og?title=Design%20Projects`,
-            author: {
-              "@type": "Person",
-              name: person.name,
-            },
-            hasPart: allProjects.map((project) => ({
-              "@type": "CreativeWork",
-              headline: project.metadata.title,
-              description: project.metadata.summary,
-              url: `https://${baseURL}/projects/${project.slug}`,
-              image: `${baseURL}/${project.metadata.image}`,
-            })),
-          }),
-        }}
-      />
-      <Projects />
-    </Column>
-  );
+    return (
+        <Column maxWidth="m">
+            <script
+                type="application/ld+json"
+                suppressHydrationWarning
+                dangerouslySetInnerHTML={{
+                    __html: JSON.stringify({
+                        "@context": "https://schema.org",
+                        "@type": "CollectionPage",
+                        headline: work.title,
+                        description: work.description,
+                        url: `https://${baseURL}/projects`,
+                        image: `${baseURL}/og?title=Design%20Projects`,
+                        author: {
+                            "@type": "Person",
+                            name: person.name,
+                        },
+                        hasPart: allProjects.map((project) => ({
+                            "@type": "CreativeWork",
+                            headline: project.metadata.title,
+                            description: project.metadata.summary,
+                            url: `https://${baseURL}/projects/${project.slug}`,
+                            image: `${baseURL}/${project.metadata.image}`,
+                        })),
+                    }),
+                }}
+            />
+            <Projects/>
+        </Column>
+    );
 }
